66f75f88098bb33132ae29e3b10891ff6e033fc9
haipv1112 <50477268+haipv1112@users.noreply.github.com>
2021-04-26 13:31:02 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 
 def register_blueprints(app):
-    for module_name in ('home', 'welcome'):
+    for module_name in ('home', 'welcome','lab'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
